029bd5af38a03f378450aeb85c8d4f2198d9de9f
johnwargo <john@johnwargo.com>
2026-02-10 19:16:48 -0500

    created project

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,77 @@
+// import { EleventyHtmlBasePlugin } from '@11ty/eleventy';
+import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
+import markdownIt from 'markdown-it';
+import markdownItAttrs from 'markdown-it-attrs';
+
+// Transforms
+import htmlMinTransform from './src/transforms/html-min.js';
+
 export default async function (eleventyConfig) {
+
+	const isProduction = process.env.NODE_ENV === 'production';
+
+	// eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+	// ==========================
+	// image plugin
+	// ==========================
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// which file extensions to process
+		extensions: 'html',
+		// optional, output image formats
+		formats: ['jpg', 'webp'],
+		// optional, output image widths
+		widths: ['auto', 400, 800, 1024],
+		// optional, attributes assigned on <img> override these values.
+		defaultAttributes: {
+			loading: 'lazy',
+			decoding: 'async',
+			class: 'image-full'
+		}
+	});
+
+	// ==========================
+	// markdown configuration
+	// ==========================
+	const mdOptions = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
+	const markdownLib = markdownIt(mdOptions)
+		.use(markdownItAttrs)
+		.disable('code');
+	eleventyConfig.setLibrary('md', markdownLib);
+
+	// ==========================
+	// utility stuff
+	// ==========================
+	eleventyConfig.addFilter('dateOnly', function (dateVal, locale = 'en-us') {
+		// Used to display human readable date on the stats page and other pages
+		var theDate = new Date(dateVal);
+		const options = {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		};
+		return theDate.toLocaleDateString(locale, options);
+	});
+
+	eleventyConfig.addFilter('readableTimestamp', function (dateVal, locale = 'en-us') {
+		// Used by home, articles, & post pages to render timestamp as human readable
+		var theDate = new Date(dateVal);
+		const options = {
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour12: true,
+			hour: '2-digit',
+			minute: '2-digit'
+		};
+		return theDate.toLocaleString(locale, options);
+	});
 
 	eleventyConfig.addPassthroughCopy({ 'src/favicon/*': '/' });
 	[
@@ -9,6 +82,8 @@ export default async function (eleventyConfig) {
 		eleventyConfig.addPassthroughCopy(path);
 	});
 
+	// Only minify HTML if we are in production
+	if (isProduction) eleventyConfig.addTransform('htmlmin', htmlMinTransform);
 
 	return {
 		dir: {
--- readme.md
+++ readme.md
@@ -1,4 +1,16 @@
-# Fumbly Workshop 11ty
+# The Fumbly Workshop (11ty)
 
 Source code project for https://fumblyworkshop.com
 
+## Tasks 
+
+- [ ] Algolia Index
+- [ ] Better search form
+- [ ] Favicon
+- [ ] Google Analytics
+- [ ] Posts section
+- [ ] News section
+- [ ] About page
+- [ ] Products section
+- [ ] 
+- [ ] 
