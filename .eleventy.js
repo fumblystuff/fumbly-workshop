6d950ce7d114e83d7bfd8ef8b474fddd03cb82af
johnwargo <john@johnwargo.com>
2026-08-05 05:54:55 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -68,9 +68,8 @@ export default async function (eleventyConfig) {
 		// optional, attributes assigned on <img> override these values.
 		defaultAttributes: {
 			loading: 'lazy',
-			decoding: 'async'
-			// ,
-			// class: 'image fit'
+			decoding: 'async',
+			class: 'image fit'
 		}
 	});
 
